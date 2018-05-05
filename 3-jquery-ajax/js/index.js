d8825c3e55e44c129df648ee0c3726a28943cc9d
Kamil Grabczak <grabczakkamil@gmail.com>
2018-05-05 15:59:14 +0200

    jquery-ajax

---
+++ 3-jquery-ajax/js/index.js
@@ -0,0 +1,12 @@
+'use strict';
+$(function(){
+   var ajaxUrl = "https://jsonplaceholder.typicode.com/posts/" 
+   
+   $.ajax({
+       url: ajaxUrl,
+       dataType: "json",
+       success: function(response) {
+           console.log(response);
+       },
+   });
+});
