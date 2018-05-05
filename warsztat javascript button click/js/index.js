dc609d219e02294e3195aad32bbb12c898fa049d
Kamil Grabczak <grabczakkamil@gmail.com>
2018-05-05 16:44:12 +0200

    warsztat javascript button click - koniec

---
+++ warsztat javascript button click/js/index.js
@@ -0,0 +1,26 @@
+'use strict';
+var btn = document.getElementById('get');
+var output = document.getElementById('output');
+
+function pobierzDane() {
+    ajax("GET", 'http://echo.jsontest.com/userId/108/userName/Akademia108/userURL/');
+    
+}
+
+function ajax(method, url) {  
+    
+    var httpReq = new XMLHttpRequest();
+    
+    httpReq.open(method, url);
+    
+    httpReq.addEventListener('readystatechange', function(){
+        if (httpReq.readyState == 4) {
+            if(httpReq.status == 200) {
+                var data = httpReq.responseText;
+                output.innerHTML = data;
+            }
+        }
+    });
+    httpReq.send();
+}
+btn.addEventListener('click', pobierzDane);
